ba047b328d0be638f4a762a6936cac3ba7543c0a
cspinetta <cebs91@gmail.com>
2018-05-25 20:29:47 -0300

    wip

--- src/test/scala/kamon/khronus/KhronusReporterSpec.scala
+++ src/test/scala/kamon/khronus/KhronusReporterSpec.scala
@@ -4,7 +4,8 @@ import com.despegar.khronus.jclient.KhronusClient
 import kamon.Kamon
 import kamon.Kamon.buildSpan
 import kamon.khronus.testkit.KhronusReporterInspector
-import kamon.testkit.Reconfigure
+import kamon.metric._
+import kamon.testkit.{MetricInspection, Reconfigure}
 import org.easymock.EasyMock.{eq => eqEM, _}
 import org.scalatest.concurrent.Eventually
 import org.scalatest.easymock.EasyMockSugar
@@ -17,6 +18,7 @@ class KhronusReporterSpec extends WordSpec
   with Eventually
   with EasyMockSugar
   with BeforeAndAfterAll
+  with MetricInspection
   with Reconfigure {
 
   override protected def beforeAll(): Unit = {
@@ -24,6 +26,9 @@ class KhronusReporterSpec extends WordSpec
       """
         |kamon {
         |  metric.tick-interval = 10 millis
+        |  trace.tick-interval = 10 millis
+        |  trace.sampler = "always"
+        |  khronus.interval = 10
         |
         |  util.filters {
         |    metric-filter {
@@ -54,7 +59,7 @@ class KhronusReporterSpec extends WordSpec
         lastCall.times(1)
         mockKhronusClient.recordGauge("test.gauge", 23)
         lastCall.times(1)
-        mockKhronusClient.recordTime("test.histogram", 1000)
+        mockKhronusClient.recordTime(eqEM("test.histogram"), gt(1))
         lastCall.times(1)
       }
 
@@ -63,11 +68,23 @@ class KhronusReporterSpec extends WordSpec
         val subscription = Kamon.addReporter(khronusReporter, name = "khronus-reporter-spec", filter = "metric-filter")
         Kamon.counter("test.counter").increment(10)
         Kamon.gauge("test.gauge").increment(23)
-        Kamon.histogram("test.histogram").record(1000)
+        val histogram = Kamon.histogram("test.histogram", MeasurementUnit.time.milliseconds)
+        histogram.record(10000, 10)
+        histogram.record(3003, 2)
+        histogram.record(3003, 5)
+        histogram.distribution(resetState = false)
+//        histogram.distribution(resetState = false)
+//        histogram.distribution(resetState = false)
+//        histogram.distribution(resetState = false).buckets.size should be (2)
 
-        eventually(timeout(20 hours)) {
-          khronusReporter.snapshotCount() should be >= 1
-          khronusReporter.metrics() should contain only ("test.counter", "test.gauge", "test.histogram")
+        try {
+
+          eventually(timeout(5 seconds)) {
+            khronusReporter.snapshotCount() should be >= 1
+            khronusReporter.metrics() should contain allOf ("test.counter", "test.gauge", "test.histogram")
+          }
+        } catch {
+          case exc: Throwable => println(s"Failed with exception: ${exc.getMessage}")
         }
 
         subscription.cancel()
